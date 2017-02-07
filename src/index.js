74794a4a81bf5505ba6e7ebeb21e77c93762877b
Vedant Pandit <vedantpandit@Vedants-MacBook-Pro.local>
2017-02-07 15:13:53 -0800

    Initial styling done, state remining

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; // importing entire library to build the applicaation
+import ReactDOM from 'react-dom';
+import App1 from './App';
+
+ReactDOM.render(
+  //<div>Countdown Champ</div>,
+  <App1 />,//<App1></App1>,
+  document.getElementById('root')
+);
