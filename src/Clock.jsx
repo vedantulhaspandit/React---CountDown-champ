f7dbbd5e08fca3e6bc7e71f14e11f071ac526b4c
Vedant Pandit <vedantpandit@Vedants-MacBook-Pro.local>
2017-02-12 12:29:25 -0800

    leading0() added for styling purposes

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -25,6 +25,10 @@ class Clock extends Component{
     setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
   }
 
+  leading0(num) {
+    return num < 10 ? '0' + num : num;
+  }
+
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
    //console.log('time',time);
@@ -41,10 +45,10 @@ class Clock extends Component{
 
     return (
        <div>
-         <div className = "Clock-days">{this.state.days} days</div>
-         <div className = "Clock-hours">{this.state.hours} hours</div>
-         <div className = "Clock-minutes">{this.state.minutes} minutes</div>
-         <div className = "Clock-seconds">{this.state.seconds} seconds</div>
+         <div className = "Clock-days">{this.leading0(this.state.days)} days</div>
+         <div className = "Clock-hours">{this.leading0(this.state.hours)} hours</div>
+         <div className = "Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+         <div className = "Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
        </div>
     )
 
