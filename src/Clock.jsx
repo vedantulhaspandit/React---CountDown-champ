756ac70b3c25c0a989f0b56b4d3a63c58810342e
Vedant Pandit <vedantpandit@Vedants-MacBook-Pro.local>
2017-02-12 12:21:09 -0800

    lifecycle methods added - componentWillMount(), componentDidMount()

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -11,23 +11,34 @@ class Clock extends Component{
       minutes: 0,
       seconds: 0
     }
-  //  console.log('this.props', this.props); // deadline = {this.state.deadline} passed from parents into props
+  //console.log('this.props', this.props); // deadline = {this.state.deadline} passed from parents into props
+  }
+
+
+ // lifecycle hook
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+ // lifecycle hook - runs after the component has completely rendered on the apllication
+  componentDidMount() { // getTimeUntil is called every second in order to continue updating the displayed timer
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
   }
 
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
-    console.log('time',time);
+   //console.log('time',time);
     const seconds = Math.floor((time/1000) % 60);
     const minutes = Math.floor((time/1000/60) % 60);
     const hours = Math.floor(time/(1000*60*60) % 24);
     const days = Math.floor(time/(1000*60*60*24));
 
-    this.setState({days, hours, minutes, seconds});
+    this.setState({days:days, hours, minutes, seconds});
   }
 
   render(){
-    this.getTimeUntil(this.props.deadline);
-    
+    //this.getTimeUntil(this.props.deadline);
+
     return (
        <div>
          <div className = "Clock-days">{this.state.days} days</div>
